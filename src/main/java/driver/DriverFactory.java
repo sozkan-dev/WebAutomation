8ce1b25ac833b2d811a62520568b09ffc32f9e07
sozkan-dev <ozkan1saricam@gmail.com>
2024-08-01 10:44:43 +0300

    selenium updated

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -12,68 +12,65 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
-	private static ThreadLocal<WebDriver> webDriver = new ThreadLocal<>();
+    private static ThreadLocal<WebDriver> webDriver = new ThreadLocal<>();
 
-	public static WebDriver getDriver() {
-		if (webDriver.get() == null) {
-			webDriver.set(createDriver());
-		}
-		return webDriver.get();
-	}
-	
-	private static String getBrowserType()  {
-		String browserType = null;
-		try {
-		Properties prop = new Properties();
-		FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/properties/config.properties");
-		prop.load(file);
-		browserType = prop.getProperty("browser").toLowerCase().trim();
-		}catch (Exception e) {
-			System.out.println(e);
-		}
-		return browserType;
-	}
-	
-	
+    public static WebDriver getDriver() {
+        if (webDriver.get() == null) {
+            webDriver.set(createDriver());
+        }
+        return webDriver.get();
+    }
 
-	private static WebDriver createDriver() {
-		WebDriver driver = null;
-		
-		
-		
-		switch (getBrowserType()) {
-		case "chrome" -> {
-			WebDriverManager.chromedriver().setup();
-			ChromeOptions options = new ChromeOptions();
-			options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-			driver = new ChromeDriver(options);
-			break;
-		}
-		case "firefox" -> {
-			WebDriverManager.firefoxdriver().setup();
-			FirefoxOptions options = new FirefoxOptions();
-			options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-			driver = new FirefoxDriver(options);
-			break;
-		}
-		case "edge" -> {
-			WebDriverManager.edgedriver().setup();
-			EdgeOptions options = new EdgeOptions();
-			options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-			driver = new EdgeDriver(options);
-			break;
-		}
-		}
-		return driver;
-	}
+    private static String getBrowserType() {
+        String browserType = null;
+        try {
+            Properties prop = new Properties();
+            FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/properties/config.properties");
+            prop.load(file);
+            browserType = prop.getProperty("browser").toLowerCase().trim();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return browserType;
+    }
 
-	public static void cleanDriver() {
-		webDriver.get().quit();
-		webDriver.remove();
-	}
+
+    private static WebDriver createDriver() {
+        WebDriver driver = null;
+
+
+        switch (getBrowserType()) {
+            case "chrome" -> {
+                //WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+                driver = new ChromeDriver(options);
+                break;
+            }
+            case "firefox" -> {
+                //WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions options = new FirefoxOptions();
+                options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+                driver = new FirefoxDriver(options);
+                break;
+            }
+            case "edge" -> {
+                //WebDriverManager.edgedriver().setup();
+                EdgeOptions options = new EdgeOptions();
+                options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+                driver = new EdgeDriver(options);
+                break;
+            }
+        }
+        return driver;
+    }
+
+    public static void cleanDriver() {
+        webDriver.get().quit();
+        webDriver.remove();
+    }
 
 }
--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -5,14 +5,14 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = { "classpath:features" }, glue = {
-		"stepDefinitions" },tags="@search-product", monochrome = true, dryRun = false, plugin = { "pretty", "html:target/cucumber.html",
-				"json:target/cucumber.json" })
+@CucumberOptions(features = {"classpath:features"}, glue = {
+        "stepDefinitions"}, tags = "@search-product", monochrome = true, dryRun = false, plugin = {"pretty", "html:target/cucumber.html",
+        "json:target/cucumber.json"})
 public class Runner extends AbstractTestNGCucumberTests {
 
-	@Override
-	@DataProvider(parallel = true)
-	public Object[][] scenarios() {
-		return super.scenarios();
-	}
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
